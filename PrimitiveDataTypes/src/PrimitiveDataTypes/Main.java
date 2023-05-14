bb940b937e0535401e54eba096ee353d24a140df
shivanadar <nrs.shiva@gmail.com>
2023-05-14 16:33:57 +0530

    PrimitiveDefaultValue

---
+++ PrimitiveDataTypes/src/PrimitiveDataTypes/Main.java
@@ -0,0 +1,20 @@
+package PrimitiveDataTypes;
+
+
+public class Main {
+	static boolean val1;
+	static double val2;
+	static float val3;
+	static int val4;
+	static long val5;
+	static String val6;
+	public static void main(String[] args) {
+		
+	      System.out.println("Boolean Default value "+ val1);
+	      System.out.println("Double Default value "+ val2);
+	      System.out.println("Float Default value "+ val3);
+	      System.out.println("Int Default value  "+ val4);
+	      System.out.println("Long Default value  "+ val5);
+	      System.out.println("String Default value  "+ val6);
+	   }
+}
